88d9a546d028d6c63514b7c73a8f6c3f8bea83a1
Ouail Laamiri <laamiri.ouail@etu.uae.ac.ma>
2023-12-25 00:00:32 +0100

    change path namming

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
+### To install requirements :
+pip install -r requirements.txt 
 ### To run app : 
 gunicorn -c gunicorn_config.py main:app
 ### Build Docker image : 
 docker build -t email-service .
 ### Run the Docker container : 
 docker run -p 5000:5000 email-service
+
--- main.py
+++ main.py
@@ -56,75 +56,75 @@ def index():
     return jsonify({"message": "Email service work"}), 200
 
 
-@app.route('/email/sentverificationcode', methods=['POST'])
+@app.route('/email/send-verification-code', methods=['POST'])
 def send_email_to_for_code_verification():
     return send_email_(os.getenv("SUBJECT_EMAIL_CODE_VERIFICATION"), os.getenv("BODY_EMAIL_CODE_VERIFICATION"), False,
                        True)
 
 
-@app.route('/email/accountpending', methods=['POST'])
+@app.route('/email/account-pending', methods=['POST'])
 def account_created_pending():
     return send_email_(os.getenv("SUBJECT_EMAIL_ACCOUNT_PENDING"), os.getenv("BODY_EMAIL_ACCOUNT_PENDING"))
 
 
-@app.route('/email/accountcreated', methods=['POST'])
+@app.route('/email/account-created', methods=['POST'])
 def account_created_successfully():
     return send_email_(os.getenv('SUBJECT_EMAIL_ACCOUNT_CREATE_SUCCESSFULLY'),
                        os.getenv("BODY_EMAIL_ACCOUNT_CREATE_SUCCESSFULLY"))
 
 
-@app.route('/email/accountnotcreated', methods=['POST'])
+@app.route('/email/account-not-created', methods=['POST'])
 def account_not_created_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_PROBLEM_IN_CREATE_ACCOUNT"),
                        os.getenv('BODY_EMAIL_PROBLEM_IN_CREATE_ACCOUNT'), True)
 
 
-@app.route('/email/viheculepending', methods=['POST'])
+@app.route('/email/vihecule-pending', methods=['POST'])
 def vihecule_created_pending():
     return send_email_(os.getenv("SUBJECT_EMAIL_VEHICLE_PENDING"), os.getenv("BODY_EMAIL_VEHICLE_PENDING"))
 
 
-@app.route('/email/viheculecreated', methods=['POST'])
+@app.route('/email/vihecule-created', methods=['POST'])
 def vihecule_created_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_VEHICLE_ADDED_SUCCESSFULLY"),
                        os.getenv("BODY_EMAIL_VEHICLE_ADDED_SUCCESSFULLY"))
 
 
-@app.route('/email/viheculenotcreated', methods=['POST'])
+@app.route('/email/vihecule-not-created', methods=['POST'])
 def vihecule_not_added_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_PROBLEM_IN_CREATE_VEHICLE"),
                        os.getenv("BODY_EMAIL_PROBLEM_IN_CREATE_VEHICLE"), True)
 
 
-@app.route('/email/permitpending', methods=['POST'])
+@app.route('/email/permit-pending', methods=['POST'])
 def permit_created_pending():
     return send_email_(os.getenv("SUBJECT_EMAIL_PERMIT_PENDING"), os.getenv("BODY_EMAIL_PERMIT_PENDING"))
 
 
-@app.route('/email/permitcreated', methods=['POST'])
+@app.route('/email/permit-created', methods=['POST'])
 def permit_created_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_PERMIT_ADDED_SUCCESSFULLY"),
                        os.getenv("BODY_EMAIL_PERMIT_ADDED_SUCCESSFULLY"))
 
 
-@app.route('/email/permitnotcreated', methods=['POST'])
+@app.route('/email/permit-not-created', methods=['POST'])
 def permit_not_added_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_PROBLEM_IN_ADD_PERMIT"), os.getenv("BODY_EMAIL_PROBLEM_IN_ADD_PERMIT"),
                        True)
 
 
-@app.route('/email/assurancepending', methods=['POST'])
+@app.route('/email/assurance-pending', methods=['POST'])
 def assurance_created_pending():
     return send_email_(os.getenv("SUBJECT_EMAIL_ASSURANCE_PENDING"), os.getenv("BODY_EMAIL_ASSURANCE_PENDING"))
 
 
-@app.route('/email/assurancecreated', methods=['POST'])
+@app.route('/email/assurance-created', methods=['POST'])
 def assurance_created_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_ASSURANCE_ADDED_SUCCESSFULLY"),
                        os.getenv("BODY_EMAIL_ASSURANCE_ADDED_SUCCESSFULLY"))
 
 
-@app.route('/email/assurancenotcreated', methods=['POST'])
+@app.route('/email/assurance-not-created', methods=['POST'])
 def assurance_not_added_successfully():
     return send_email_(os.getenv("SUBJECT_EMAIL_PROBLEM_IN_ADD_ASSURANCE"),
                        os.getenv("BODY_EMAIL_PROBLEM_IN_ADD_ASSURANCE"), True)
